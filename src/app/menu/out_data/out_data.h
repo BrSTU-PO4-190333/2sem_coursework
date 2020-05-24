7ebb6fcaffb88f9dbecdd62f451b2dcdafdd11b0
Pavel Galanin 2001 <PavelGalanin2001@outlook.com>
2020-05-25 02:36:11 +0300

    delete old

--- src/app/menu/out_data/out_data.h
+++ src/app/menu/out_data/out_data.h
@@ -9,7 +9,6 @@
     #include "../../../my_libs/pause_console/pause_console.h"
     #include "../../../my_libs/clear_console/clear_console.h"
     #include "../../../my_libs/indices_array/get_indices_array_from_file/get_indices_array_from_file.h"
-    #include "write_table/write_table.h"
     #include "print_sep/print_sep.h"
 
     void out_data(struct reservoirs_of_the_country* array, int length);
